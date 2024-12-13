4b17023caab709356da7cf0e3fbc1c2da833513f
Rickyash <1145967792@qq.com>
2024-12-13 07:39:08 -0500

    Update main.py

--- impl/main.py
+++ impl/main.py
@@ -1,11 +1,27 @@
 # main.py
-
 import sys
-from main_scene import main_scene
 
 def main():
-    gif_path = "../res/gifs/city.gif"
-    main_scene(gif_path)
+    print("Please select a scene to run:")
+    print("1 - Main Scene")
+    print("2 - Music Scene")
+    print("3 - Chat Scene")
+    choice = input("Enter your choice (1/2/3): ")
+
+    if choice == "1":
+        from main_scene import main_scene
+        gif_path = "../res/gifs/city.gif"
+        main_scene(gif_path)
+    elif choice == "2":
+        from music_scene import music_scene
+        song_id = 1372554118
+        music_scene(song_id)
+    elif choice == "3":
+        from chat_scene import chat_scene
+        chat_scene()
+        
+    else:
+        print("Invalid choice. Please run the program again and select 1, 2, or 3.")
 
 if __name__ == "__main__":
     main()
